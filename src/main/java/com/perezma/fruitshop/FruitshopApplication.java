8b1f4d4064b1be0c3e8970e7b65d3fedf35c4d84
michaelperez <michael.perez@yahoo.com>
2019-05-25 11:20:00 -0500

    Initial import

---
+++ src/main/java/com/perezma/fruitshop/FruitshopApplication.java
@@ -0,0 +1,13 @@
+package com.perezma.fruitshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FruitshopApplication.class, args);
+    }
+
+}
