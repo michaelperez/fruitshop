e6adb17c2acfc4e0c5891e4fc8138b67c5dc737f
michaelperez <michael.perez@yahoo.com>
2019-05-27 07:41:23 -0500

    Add Category repository that extends JpaRepository. closes #3

---
+++ src/main/java/com/perezma/fruitshop/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.perezma.fruitshop.repositories;
+
+import com.perezma.fruitshop.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
